54f2b1878b5ff8dc5d184917bd1fcbe24698be24
Darshan <darshanc4696@gmail.com>
2025-01-03 16:43:41 +0530

    Archietecture Created

---
+++ src/main/java/com/bank/BiggyBank/BiggyBankApplication.java
@@ -0,0 +1,16 @@
+package com.bank.BiggyBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiggyBankApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(BiggyBankApplication.class, args);
+		System.out.println("Applictaion Started");
+		
+	}
+
+}
---
+++ src/test/java/com/bank/BiggyBank/BiggyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.BiggyBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiggyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
